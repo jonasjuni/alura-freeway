4bac6ae1a0a3e8c44d59bb727dfa6b981d275004
Jonas Junior <jonas.developer@live.com>
2024-04-25 11:53:55 -0300

    code refactor add loops

--- cars.js
+++ cars.js
@@ -1,36 +1,29 @@
 //cars
-let xCar1 = 600;
-let yCar1 = 40;
-let vCar1 = 2;
 
-let xCar2 = 600;
-let yCar2 = 98;
-let vCar2 = 2.5;
+let xCars = [600, 600, 600];
+let yCars = [40, 98, 150];
+let vCars = [2, 2.5, 3.2];
 
-let xCar3 = 600;
-let yCar3 = 150;
-let vCar3 = 3.2;
 
 function drawCars() {
-    image(car1Sprite, xCar1, yCar1, 50, 40);
-    image(car2Sprite, xCar2, yCar2, 50, 40);
-    image(car3Sprite, xCar3, yCar3, 50, 40);
+    for (let i = 0; i < carsSprites.length; i++) {
+        image(carsSprites[i], xCars[i], yCars[i], 50, 40);
+    }
 }
 
 function moveCars() {
-    xCar1 -= vCar1;
-    xCar2 -= vCar2;
-    xCar3 -= vCar3;
+    for (let i = 0; i < carsSprites.length; i++) {
+        xCars[i] -= vCars[i];
+    }
 }
 
 function resetCars() {
-    if (xCar1 < -50) {
-        xCar1 = 600;
+    for(let i = 0; i < carsSprites.length; i++) {
+        if (checkEndPositon(xCars[i])) {
+            xCars[i] = 600;
+        }   
     }
-    if (xCar2 < -50) {
-        xCar2 = 600;
-    }
-    if (xCar3 < -50) {
-        xCar3 = 600;
-    }
+}
+function checkEndPositon(position) {
+    return position < -50;
 }
--- images.js
+++ images.js
@@ -2,14 +2,18 @@
 
 let backgroundImage;
 let playerSprite;
+
+let car0Sprite;
 let car1Sprite;
 let car2Sprite;
-let car3Sprite;
+let carsSprites;
 
 function preload() {
     backgroundImage = loadImage('assets/images/estrada.png');
     playerSprite = loadImage('assets/images/ator-1.png');
-    car1Sprite = loadImage('assets/images/carro-1.png');
-    car2Sprite = loadImage('assets/images/carro-2.png');
-    car3Sprite = loadImage('assets/images/carro-3.png');
+    car0Sprite = loadImage('assets/images/carro-1.png');
+    car1Sprite = loadImage('assets/images/carro-2.png');
+    car2Sprite = loadImage('assets/images/carro-3.png');
+
+    carsSprites = [car0Sprite, car1Sprite,car2Sprite]
 }
